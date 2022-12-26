96598d0dbcc7d1418fa099771e32443baa24d0ba
Mevlut Doger <mdoger@hotmail.com>
2022-12-26 15:58:55 +0000

    Added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to GitHub");
+        System.out.println("Doing more work");
 
     }
 }
